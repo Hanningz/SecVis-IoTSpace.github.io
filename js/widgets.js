b5c89967ce1eaa062d314595a5e75a34245252ca
Hanningz <xiongqionghai@gmail.com>
2021-07-07 14:04:05 +0300

    Update widgets.js

--- js/widgets.js
+++ js/widgets.js
@@ -1,12 +1,3 @@
-/*=========================================================================================
-    File Name: widgets.js
-    Description: widgets page content with different types of cards
-    ----------------------------------------------------------------------------------------
-    Item Name: Frest HTML Admin Template
-    Version: 1.0
-    Author: PIXINVENT
-    Author URL: http://www.themeforest.net/user/pixinvent
-==========================================================================================*/
 
 $(document).ready(function () {
 
